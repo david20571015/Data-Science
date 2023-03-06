b1b4af1b3b54b5f61c396308f7a2a435c7057f44
David Chiu <david20571015@gmail.com>
2023-03-06 08:49:11 +0800

    feat: crawl

--- hw1/311511038.py
+++ hw1/311511038.py
@@ -1,6 +1,6 @@
 import asyncio
+from collections import Counter
 from datetime import datetime
-from datetime import timedelta
 from functools import reduce
 import json
 import operator
@@ -30,8 +30,8 @@ def cli():
 
 
 @cli.command(short_help='Crawl all articles in 2022 from ptt')
-@click.option('--use-async', '-a', is_flag=True, help='Use async to crawl')
-def crawl(use_async):
+@click.option('--use-async/--no-async', default=True, help='Use async to crawl')
+def crawl(use_async: bool):
     """Crawl all articles in 2022 from ptt.
 
     Export two json files: all_article.jsonl and all_popular.jsonl.
@@ -40,10 +40,12 @@ def crawl(use_async):
     `{"date": {date}, "title": {title}, "url": {url}}`.
     """
     billboard = 'Beauty'
-    indexes = range(3647, 3955 + 1)
+    indexes = range(3500, 3960)  # TODO: must be computed dynamically
     year = 2022
 
     host = 'https://www.ptt.cc'
+
+    session = requests.Session()
 
     def parse_article_elememt(article_element):
         title_element = article_element.select_one('div.title a')
@@ -63,16 +65,19 @@ def crawl(use_async):
     end_date = datetime.strptime(f'{year}1231 23:59:59', '%Y%m%d %H:%M:%S')
 
     def check_available_date(url: str) -> bool:
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        date_element = soup.select('span.article-meta-value')[-1]
-        date = datetime.strptime(date_element.text, '%c')
+        response = session.get(url, headers=HEADERS, timeout=10)
+        soup = BeautifulSoup(response.text, 'lxml')
+        try:
+            date_element = soup.select('span.article-meta-value')[-1]
+            date = datetime.strptime(date_element.text, '%c')
+        except Exception:
+            return False
         return start_date <= date <= end_date
 
     def crawl_one_page(url: str):
         article_infos = []
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        response = session.get(url, headers=HEADERS, timeout=10)
+        soup = BeautifulSoup(response.text, 'lxml')
         article_elements = soup.select('div.r-ent')
         for article_element in article_elements:
             article_info, is_popular = parse_article_elememt(article_element)
@@ -82,15 +87,18 @@ def crawl(use_async):
             if (article_info['title'].startswith('[公告]') or
                     article_info['title'].startswith('Fw: [公告]')):
                 continue
-            if (article_info['date'] in ['0101', '1231'] and
-                    not check_available_date(article_info['url'])):
-                continue
 
             article_infos.append((article_info, is_popular))
 
         return article_infos
 
     def dump_article_infos(article_infos: list[tuple[dict, bool]]):
+        # remove articles that are not in 2022
+        while not article_infos[0][0]['date'] == '0101':
+            article_infos.pop(0)
+        while not article_infos[-1][0]['date'] == '1231':
+            article_infos.pop()
+
         with open(
                 ALL_ARTICLE_FILE,
                 'w',
@@ -141,64 +149,263 @@ def parse_date(date: str) -> datetime:
     return datetime.strptime(date, '%m%d')
 
 
+def filter_date_interval(article_infos: list, start_date: str, end_date: str):
+    start_datetime = parse_date(start_date)
+    end_datetime = parse_date(end_date)
+    return filter(
+        lambda x: start_datetime <= parse_date(x['date']) <= end_datetime,
+        article_infos,
+    )
+
+
+def load_article_infos(
+    filename: str,
+    start_date: str = '0101',
+    end_date: str = '1231',
+):
+    _check_file_exists(filename)
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        article_infos = [json.loads(line) for line in file]
+
+    article_infos = filter_date_interval(article_infos, start_date, end_date)
+    return article_infos
+
+
 @cli.command(short_help='Compute the top 10 user of push/boo')
-@click.argument('start-date', type=parse_date)
-@click.argument('end-date', type=parse_date)
-def push(start_date: datetime, end_date: datetime):
-    """Compute the top 10 user of push/boo from START_DATE to END_DATE.
+@click.argument('start-date', type=str)
+@click.argument('end-date', type=str)
+@click.option('--use-async/--no-async', default=True, help='Use async to crawl')
+def push(start_date: str, end_date: str, use_async: bool):
+    """Compute the top 10 user of like/boo from START_DATE to END_DATE.
 
     \b
     Args:
-        start_date (datetime): Start date.
-        end_date (datetime): End date.
+        start_date (str): Start date.
+        end_date (str): End date.
     """
-    _check_file_exists(ALL_ARTICLE_FILE)
+    article_infos = load_article_infos(ALL_ARTICLE_FILE, start_date, end_date)
 
-    with open(ALL_ARTICLE_FILE, 'r', encoding='utf-8') as all_article_file:
-        article_infos = [json.loads(line) for line in all_article_file]
-
-    article_infos = filter(
-        lambda x: start_date <= parse_date(x['date']) <= end_date,
-        article_infos)
-
-    urls = [article_info['url'] for article_info in article_infos]
+    session = requests.Session()
 
     def crawl_one_page(url: str):
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        response = session.get(url, headers=HEADERS, timeout=10)
+        soup = BeautifulSoup(response.text, 'lxml')
         push_elements = soup.select('div.push')
-        return [push_element.text for push_element in push_elements]
+
+        like_list, boo_list = [], []
+        for push_element in push_elements:
+            push_tag = push_element.select_one('span.push-tag').text.strip()
+            push_userid = push_element.select_one(
+                'span.push-userid').text.strip()
+            if push_tag == '推':
+                like_list.append(push_userid)
+            elif push_tag == '噓':
+                boo_list.append(push_userid)
+
+        return {
+            'all_like': len(like_list),
+            'all_boo': len(boo_list),
+            'likes': Counter(like_list),
+            'boos': Counter(boo_list),
+        }
+
+    def dump_like_boo_info(like_boo_info: dict):
+        result = {
+            'all_like': like_boo_info['all_like'],
+            'all_boo': like_boo_info['all_boo'],
+        }
+
+        top_10_likes = sorted(
+            like_boo_info['likes'].most_common(),
+            key=operator.itemgetter(1, 0),  # sort by count, then by user_id
+            reverse=True,
+        )[:10]
+        for rank, (uid, count) in enumerate(top_10_likes):
+            result[f'like {rank+1}'] = {'user_id': uid, 'count': count}
+
+        top_10_boos = sorted(
+            like_boo_info['boos'].most_common(),
+            key=operator.itemgetter(1, 0),  # sort by count, then by user_id
+            reverse=True,
+        )[:10]
+        for rank, (uid, count) in enumerate(top_10_boos):
+            result[f'boo {rank+1}'] = {'user_id': uid, 'count': count}
+
+        like_boo_file = f'push_{start_date}_{end_date}.json'
+        with open(like_boo_file, 'w', encoding='utf-8') as file:
+            json.dump(result, file, ensure_ascii=False)
+
+    def add_like_boo_info(info1: dict, info2: dict):
+        return {
+            'all_like': info1['all_like'] + info2['all_like'],
+            'all_boo': info1['all_boo'] + info2['all_boo'],
+            'likes': info1['likes'] + info2['likes'],
+            'boos': info1['boos'] + info2['boos'],
+        }
+
+    def sync_crawl(urls):
+        results = [crawl_one_page(url) for url in tqdm(urls)]
+
+        like_boo_info = reduce(add_like_boo_info, results, {
+            'all_like': 0,
+            'all_boo': 0,
+            'likes': Counter(),
+            'boos': Counter(),
+        })
+        dump_like_boo_info(like_boo_info)
+
+    async def async_crawl(urls):
+        results = await tqdm_asyncio.gather(
+            *[asyncio.to_thread(crawl_one_page, url) for url in urls])
+
+        like_boo_info = reduce(add_like_boo_info, results, {
+            'all_like': 0,
+            'all_boo': 0,
+            'likes': Counter(),
+            'boos': Counter(),
+        })
+        dump_like_boo_info(like_boo_info)
+
+    urls = [article_info['url'] for article_info in article_infos]
+    if use_async:
+        asyncio.run(async_crawl(urls))
+    else:
+        sync_crawl(urls)
 
 
 @cli.command(short_help='Find popular articles and its image urls')
-@click.argument('start-date', type=parse_date)
-@click.argument('end-date', type=parse_date)
-def popular(start_date: datetime, end_date: datetime):
+@click.argument('start-date', type=str)
+@click.argument('end-date', type=str)
+@click.option('--use-async/--no-async', default=True, help='Use async to crawl')
+def popular(start_date: str, end_date: str, use_async: bool):
     """Find popular articles and its image urls from START_DATE to END_DATE.
 
     \b
     Args:
-        start_date (datetime): Start date.
-        end_date (datetime): End date.
+        start_date (str): Start date.
+        end_date (str): End date.
     """
-    _check_file_exists(ALL_POPULAR_FILE)
+    article_infos = load_article_infos(ALL_POPULAR_FILE, start_date, end_date)
+
+    session = requests.Session()
+
+    def is_image_url(url):
+        prefix = ('http://', 'https://')
+        suffix = ('jpg', 'jpeg', 'png', 'gif')
+        return url.startswith(prefix) and url.endswith(suffix)
+
+    def crawl_one_page(url: str):
+        response = session.get(url, headers=HEADERS, timeout=10)
+        soup = BeautifulSoup(response.text, 'lxml')
+        image_elements = soup.select('div#main-content a')
+
+        image_urls = [
+            i.get('href') for i in image_elements if is_image_url(i.get('href'))
+        ]
+        return image_urls
+
+    def dump_image_urls(num_articles: int, image_urls: list):
+        result = {
+            'number_of_popular_articles': num_articles,
+            'image_urls': image_urls,
+        }
+        image_urls_file = f'popular_{start_date}_{end_date}.json'
+        with open(image_urls_file, 'w', encoding='utf-8') as file:
+            json.dump(result, file, ensure_ascii=False)
+
+    def sync_crawl(urls):
+        results = [crawl_one_page(url) for url in tqdm(urls)]
+        image_urls = reduce(operator.add, results, [])
+        dump_image_urls(len(urls), image_urls)
+
+    async def async_crawl(urls):
+        results = await tqdm_asyncio.gather(
+            *[asyncio.to_thread(crawl_one_page, url) for url in urls])
+        image_urls = reduce(operator.add, results, [])
+        dump_image_urls(len(urls), image_urls)
+
+    urls = [article_info['url'] for article_info in article_infos]
+    if use_async:
+        asyncio.run(async_crawl(urls))
+    else:
+        sync_crawl(urls)
 
 
 @cli.command(short_help='Find the images of the article contains the keyword')
-@click.argument('keyword', type=str)
-@click.argument('start-date', type=parse_date)
-@click.argument('end-date', type=parse_date)
-def keyword(keyword: str, start_date: datetime, end_date: datetime):
+@click.argument('kw', type=str)
+@click.argument('start-date', type=str)
+@click.argument('end-date', type=str)
+@click.option('--use-async/--no-async', default=True, help='Use async to crawl')
+def keyword(kw: str, start_date: str, end_date: str, use_async: bool):
     """Find the images of the article contains the KEYWORD from START_DATE to
     END_DATE.
 
     \b
     Args:
-        keyword (str): Keyword.
-        start_date (datetime): Start date.
-        end_date (datetime): End date.
+        kw (str): Keyword.
+        start_date (str): Start date.
+        end_date (str): End date.
     """
-    _check_file_exists(ALL_ARTICLE_FILE)
+    article_infos = load_article_infos(ALL_ARTICLE_FILE, start_date, end_date)
+
+    session = requests.Session()
+
+    def contains_keyword(dom: BeautifulSoup):
+        ele_list = dom.select('div#main-content span.f2')
+        if not ele_list:
+            return False
+        if not any('發信站' in i.text for i in ele_list):
+            return False
+
+        contents = list(dom.select_one('div#main-content').children)
+        end_idx = contents.index(ele_list[0])
+        text = [i.text for i in contents[0:end_idx]]
+
+        return any(kw in t for t in text)
+
+    def is_image_url(url):
+        prefix = ('http://', 'https://')
+        suffix = ('jpg', 'jpeg', 'png', 'gif')
+        return url.startswith(prefix) and url.endswith(suffix)
+
+    def crawl_one_page(url: str):
+        response = session.get(url, headers=HEADERS, timeout=10)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        if not contains_keyword(soup):
+            return []
+
+        image_elements = soup.select('div#main-content a')
+        image_urls = [
+            i.get('href') for i in image_elements if is_image_url(i.get('href'))
+        ]
+        return image_urls
+
+    def dump_image_urls(image_urls: list):
+        result = {
+            'image_urls': image_urls,
+        }
+        image_urls_file = f'keyword_{kw}_{start_date}_{end_date}.json'
+        with open(image_urls_file, 'w', encoding='utf-8') as file:
+            json.dump(result, file, ensure_ascii=False)
+
+    def sync_crawl(urls):
+        results = [crawl_one_page(url) for url in tqdm(urls)]
+        image_urls = reduce(operator.add, results, [])
+        dump_image_urls(image_urls)
+
+    async def async_crawl(urls):
+        results = await tqdm_asyncio.gather(
+            *[asyncio.to_thread(crawl_one_page, url) for url in urls])
+        image_urls = reduce(operator.add, results, [])
+        dump_image_urls(image_urls)
+
+    urls = [article_info['url'] for article_info in article_infos]
+    if use_async:
+        asyncio.run(async_crawl(urls))
+    else:
+        sync_crawl(urls)
 
 
 if __name__ == '__main__':
